20585bcbc15fc5a727f5d55962ca08e1f97d0c91
Fatih_ASA <fatihasa96@gmail.com>
2020-11-05 15:05:26 +0100

    //

--- app/src/main/java/com/fatih/writingreadindfile/MainActivity.java
+++ app/src/main/java/com/fatih/writingreadindfile/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fatih.writingreadindfile;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +20,20 @@ public class MainActivity extends AppCompatActivity {
         try{
             FileOutputStream fos = openFileOutput("File",MODE_PRIVATE);
             String s = "File written";
-
-
+            fos.write(s.getBytes());
+            fos.close();
 
         }catch (Exception e){
 
         }
 
         try{
+            FileInputStream fis = openFileInput("File");
+            InputStreamReader isr = new InputStreamReader(fis);
+            TextView textView = findViewById(R.id.hello);
+            BufferedReader bf =new BufferedReader(isr);
+            textView.setText(bf.readLine());
+
 
         }catch (Exception e){
 
